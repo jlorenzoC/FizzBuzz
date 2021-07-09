b0404f1d69f199688d14a0f69774a25ca21ccc4b
Jorge Javier Lorenzo Columbié <jacksparrow2017.11.30@gmail.com>
2021-07-09 01:42:06 -0400

    feat(fizzbuzz pipe)
    Add fizzbuzz pipe using TDD

---
+++ src/app/fizzbuzz.pipe.spec.ts
@@ -0,0 +1,49 @@
+import { FizzbuzzPipe } from './fizzbuzz.pipe';
+
+describe('FizzbuzzPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FizzbuzzPipe();
+    expect(pipe).toBeTruthy();
+  });
+
+  it('should return 1', () => {
+    const pipe = new FizzbuzzPipe();
+    expect(pipe.transform(1)).toEqual(1);
+  });
+
+  it('should return 2', () => {
+    const pipe = new FizzbuzzPipe();
+    expect(pipe.transform(2)).toEqual(2);
+  });
+
+  it('shouldn\'t return 3 for 3', () => {
+    const pipe = new FizzbuzzPipe();
+    expect(pipe.transform(3)).not.toEqual(3);
+  });
+
+  it('should return Fizz for 3', () => {
+    const pipe = new FizzbuzzPipe();
+    expect(pipe.transform(3)).toEqual('Fizz');
+  });
+
+  it('should return Fizz for multiples of 3', () => {
+    const pipe = new FizzbuzzPipe();
+    expect(pipe.transform(6)).toEqual('Fizz');
+    expect(pipe.transform(9)).toEqual('Fizz');
+    expect(pipe.transform(12)).toEqual('Fizz');
+  });
+
+  it('should return Buzz for multiples of 5', () => {
+    const pipe = new FizzbuzzPipe();
+    expect(pipe.transform(5)).toEqual('Buzz');
+    expect(pipe.transform(10)).toEqual('Buzz');
+    expect(pipe.transform(100)).toEqual('Buzz');
+  });
+
+  it('should return FizzBuzz for multiples of 3 and 5', () => {
+    const pipe = new FizzbuzzPipe();
+    expect(pipe.transform(15)).toEqual('FizzBuzz');
+    expect(pipe.transform(45)).toEqual('FizzBuzz');
+    expect(pipe.transform(75)).toEqual('FizzBuzz');
+  });
+});
