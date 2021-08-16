0344e3c03fdaaec71c30bf9a5c2619d651e601f7
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-08-16 12:47:24 -0400

    :white_check_mark: add first cypress test

---
+++ cypress/integration/1-fizzbuzz/fizzbuzz.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('FizzBuzz tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('Should exist div with text FizzBuzz', () => {
+    cy.get('div.p-card-title').contains('FizzBuzz').should('exist');
+  });
+  it('Should exist div with text Which number is Fizz, Buzz, or FizzBuzz?', () => {
+    cy.get('div.p-card-subtitle').contains('FizzBuzz').should('exist');
+  });
+  it('Should exist 100 fizzbuzz cards', () => {
+    cy.get('p-card.fizzbuzz-card', { timeout: 10000 }).should(
+      'have.length',
+      100
+    );
+  });
+  it('Should exist card with the number 1 as title', () => {
+    cy.get('input', { timeout: 10000 }).should('be.enabled').type(1);
+
+    cy.get('p-card.fizzbuzz-card').should('have.length', 20);
+
+    cy.get('p-card.fizzbuzz-card[data-key=1]').should('have.text', ' 1 ');
+  });
+});
