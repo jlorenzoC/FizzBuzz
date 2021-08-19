c7c2021bed9faa8858895274be1a54daba45f9ad
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-08-19 07:53:28 -0400

    :white_check_mark: updates test
    update test according to new changes

--- cypress/integration/1-fizzbuzz/fizzbuzz.spec.js
+++ cypress/integration/1-fizzbuzz/fizzbuzz.spec.js
@@ -5,10 +5,14 @@ describe('FizzBuzz tests', () => {
     cy.visit('/');
   });
   it('Should exist div with text FizzBuzz', () => {
-    cy.get('div.p-card-title').contains('FizzBuzz').should('exist');
+    cy.get('div.p-card-title', { timeout: 10000 })
+      .contains('FizzBuzz')
+      .should('exist');
   });
   it('Should exist div with text Which number is Fizz, Buzz, or FizzBuzz?', () => {
-    cy.get('div.p-card-subtitle').contains('FizzBuzz').should('exist');
+    cy.get('div.p-card-subtitle', { timeout: 10000 })
+      .contains('FizzBuzz')
+      .should('exist');
   });
   it('Should exist 100 fizzbuzz cards', () => {
     cy.get('p-card.fizzbuzz-card', { timeout: 10000 }).should(
@@ -19,7 +23,10 @@ describe('FizzBuzz tests', () => {
   it('Should exist card with the number 1 as title', () => {
     cy.get('input', { timeout: 10000 }).should('be.enabled').type(1);
 
-    cy.get('p-card.fizzbuzz-card').should('have.length', 20);
+    cy.get('p-card.fizzbuzz-card', { timeout: 10000 }).should(
+      'have.length',
+      20
+    );
 
     cy.get('p-card.fizzbuzz-card[data-key=1]').should('have.text', ' 1 ');
   });
