b77a4f69af169429d941aa97f125fd6ffc7fdc74
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-08-19 08:02:22 -0400

    :sparkles: uses Angular animation
    animate the main filter card: it will desappear and appear softly depending on the scrolling value of the browser document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 import { interval, Subscription } from 'rxjs';
@@ -7,6 +14,23 @@ import { take } from 'rxjs/operators';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('hideFilterCard', [
+      state(
+        'hide',
+        style({
+          opacity: 0,
+        })
+      ),
+      state(
+        'show',
+        style({
+          opacity: 1,
+        })
+      ),
+      transition('* => *', animate(150)),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'FizzBuzz';
