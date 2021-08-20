3f28afef5d1badb5143b5d82f204171a55a79c6f
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-08-20 13:34:53 -0400

    :fire: deletes files
    Deletes scss and html files. Because template is not so big, it can be moved as inline component template at the ts file.

--- src/app/components/skeleton-fzbz-filter-card/skeleton-fzbz-filter-card.component.ts
+++ src/app/components/skeleton-fzbz-filter-card/skeleton-fzbz-filter-card.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-skeleton-fzbz-filter-card',
-  templateUrl: './skeleton-fzbz-filter-card.component.html',
-  styleUrls: ['./skeleton-fzbz-filter-card.component.scss'],
+  template: `
+    <p-skeleton
+      height="2rem"
+      width="35%"
+      styleClass="my-2"
+      borderRadius="16px"
+    ></p-skeleton>
+    <p-skeleton
+      width="70%"
+      styleClass="mt-2 mb-4"
+      borderRadius="16px"
+    ></p-skeleton>
+    <p-skeleton
+      styleClass="mt-2"
+      height="3rem"
+      borderRadius="16px"
+    ></p-skeleton>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SkeletonFzbzFilterCardComponent {}
