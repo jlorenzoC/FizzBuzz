063e7f44ed1c3483ed179c34df91e0359ef0e9e6
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-08-21 07:25:41 -0400

    :sparkles: statistic component
    shows the statistic component side by side with skeleton, which allows to see the progress of the amount of elements that are being processed while the elements are being generated.

--- src/app/components/skeleton-fzbz-filter-card/skeleton-fzbz-filter-card.component.ts
+++ src/app/components/skeleton-fzbz-filter-card/skeleton-fzbz-filter-card.component.ts
@@ -1,14 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-skeleton-fzbz-filter-card',
   template: `
-    <p-skeleton
-      height="2rem"
-      width="35%"
-      styleClass="my-2"
-      borderRadius="16px"
-    ></p-skeleton>
+    <div class="flex justify-content-between">
+      <p-skeleton
+        id="title-skeleton"
+        height="2rem"
+        styleClass="my-2"
+        borderRadius="16px"
+      ></p-skeleton>
+      <app-fzbz-statistic-area
+        [filteredElements]="filteredElements"
+      ></app-fzbz-statistic-area>
+    </div>
     <p-skeleton
       width="70%"
       styleClass="mt-2 mb-4"
@@ -20,6 +25,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       borderRadius="16px"
     ></p-skeleton>
   `,
+  styles: [
+    `
+      :host::ng-deep #title-skeleton {
+        width: 50%;
+        max-width: 200px;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SkeletonFzbzFilterCardComponent {}
+export class SkeletonFzbzFilterCardComponent {
+  @Input() filteredElements!: number[];
+}
