ecb8ee0f18d525c838f8cfc4f357c479e11da3ad
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-08-20 20:44:49 -0400

    :art: moves inline template to a new file

--- src/app/components/input-search-by-number/input-search-by-number.component.ts
+++ src/app/components/input-search-by-number/input-search-by-number.component.ts
@@ -8,44 +8,7 @@ import {
 
 @Component({
   selector: 'app-input-search-by-number',
-  template: `
-    <div *ngIf="showContent; else skeleton">
-      <div *ngIf="showTitle">
-        <div class="p-card-title">FizzBuzz</div>
-        <div class="p-card-subtitle">
-          Which number is Fizz, Buzz, or FizzBuzz?
-        </div>
-      </div>
-      <span class="p-input-icon-right w-full margin-top16">
-        <i class="pi pi-search "></i>
-        <input
-          [id]="id"
-          #input
-          (input)="emitter.emit(input.value)"
-          [ngModel]="filterValue"
-          (ngModelChange)="filterValueChange.emit($event)"
-          pInputText
-          placeholder="Search by number ..."
-          pKeyFilter="int"
-          class="w-full"
-          st
-        />
-      </span>
-    </div>
-    <ng-template #skeleton>
-      <app-skeleton-fzbz-filter-card
-        *ngIf="showTitle; else skeletonWithoutTitleSkeleton"
-      ></app-skeleton-fzbz-filter-card>
-      <ng-template #skeletonWithoutTitleSkeleton>
-        <p-skeleton
-          class="w-7"
-          styleClass="my-2"
-          height="3rem"
-          borderRadius="16px"
-        ></p-skeleton>
-      </ng-template>
-    </ng-template>
-  `,
+  templateUrl: 'input-search-by-number.component.html',
   styles: [
     `
       .margin-top16 {
@@ -59,7 +22,8 @@ export class InputSearchByNumberComponent {
   @Input() showTitle = false;
   @Input() id!: string;
   @Input() showContent!: boolean;
-  @Output() emitter = new EventEmitter<string>();
+  @Output() value = new EventEmitter<string>();
   @Input() filterValue!: number;
   @Output() filterValueChange = new EventEmitter<number>();
+  @Input() filteredElements!: number[];
 }
