ca1dff2627315f1cae3ffcd3213853d5ec2ccb27
Javier Lorenzo <jlor3nzoc@gmail.com>
2025-07-10 22:26:31 -0400

    refactor: invert condition and inline return in highlight()

--- src/app/directive/highlight.directive.ts
+++ src/app/directive/highlight.directive.ts
@@ -7,7 +7,8 @@ export class HighlightDirective implements OnInit {
   @Input() numberToHighlight: string = '-1';
   @Input() searchCriteriaNumberToCompareTo: string = '-1';
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+  
   ngOnInit(): void {
     this.renderer[this.highlight() ? 'addClass' : 'removeClass'](
       this.elementRef.nativeElement,
@@ -16,11 +17,9 @@ export class HighlightDirective implements OnInit {
   }
 
   private highlight(): boolean {
-    if (this.searchCriteriaNumberToCompareTo !== '') {
-      return this.numberToHighlight.includes(
-        this.searchCriteriaNumberToCompareTo
-      );
-    }
-    return false;
+    if (this.searchCriteriaNumberToCompareTo == '') return false;
+    return this.numberToHighlight.includes(
+      this.searchCriteriaNumberToCompareTo
+    );
   }
 }
